62cbabd458d30713a365565e5b5fc6a162af84e5
aweakdog <yliog@connect.ust.hk>
2025-06-30 01:41:59 +0800

    feat/fisher

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -383,6 +383,7 @@ class ActorRolloutRefWorker(Worker):
             self.fisher_info_analyzer = None
             if self.config.actor.get("fisher_analysis_enabled", True):
                 if self.config.actor.get('fsdp_component_analysis', {}).get('run_fisher_info_analysis', True):
+                    from verl.utils.redo_utils.fisher_info_analyzer import FisherInfoAnalyzer
                     self.fisher_info_analyzer = FisherInfoAnalyzer.remote(self.config)
 
             self.actor = DataParallelPPOActor(

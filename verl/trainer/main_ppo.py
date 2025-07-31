7eb2f4d780e703233cdf9eacc213f8dbe04e27a2
aweakdog <yliog@connect.ust.hk>
2025-07-31 19:09:33 +0800

    feat/scripts

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -121,10 +121,7 @@ def main(config):
         # If no external Ray cluster, configure resources explicitly
         if not os.environ.get("RAY_ADDRESS"):
             print(f"[Ray Init] Starting local Ray cluster with {num_gpus} GPUs")
-            ray_config["_system_config"] = {
-                "object_store_memory": 1000000000,  # 1GB
-                "automatic_object_store_memory_fallback": False,
-            }
+            ray_config["object_store_memory"] = 1000000000  # 1GB object store
         
         ray.init(**ray_config)
         

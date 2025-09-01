a52dc0714056831573a6f4adecb8ef226e0761a4
aweakdog <yliog@connect.ust.hk>
2025-09-01 22:44:29 +0800

    feat/data

--- scripts_all/base/qwen/continual/temp_continual.sh
+++ scripts_all/base/qwen/continual/temp_continual.sh
@@ -6,7 +6,7 @@ EXPERIMENT_TYPE=${1:-train}  # Default to 'train' if no argument provided
 # Model configuration parameters
 SFT_MODEL_BASE_DIR=${SFT_MODEL_BASE_DIR:-"./models"}
 SFT_MODEL_NAME=${SFT_MODEL_NAME:-"qwen_base_sft_models"}
-SFT_CHECKPOINT=${SFT_CHECKPOINT:-"global_step_0"}
+SFT_CHECKPOINT=${SFT_CHECKPOINT:-"global_step_5"}
 
 echo "[Experiment Type] Using type: $EXPERIMENT_TYPE"
 echo "[Model Config] SFT model base directory: $SFT_MODEL_BASE_DIR"
@@ -294,9 +294,9 @@ python3 -m verl.trainer.main_ppo \
   critic.ppo_mini_batch_size=32 \
   critic.ppo_micro_batch_size=8 \
   ++actor_rollout_ref.actor.redo_tau=0.1 \
-  ++actor_rollout_ref.actor.enable_gradient_analysis=true \
+  ++actor_rollout_ref.actor.enable_gradient_analysis=false \
   ++actor_rollout_ref.actor.gradient_analysis_freq=1 \
-  ++actor_rollout_ref.actor.enable_fisher_analysis=true \
+  ++actor_rollout_ref.actor.enable_fisher_analysis=false \
   ++actor_rollout_ref.actor.fisher_analysis_freq=1 \
   algorithm.kl_ctrl.kl_coef=0.001 \
   trainer.logger=['wandb','console'] \
